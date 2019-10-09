82373e75d42ec203f7eb2658dec81d7a9d6069d1
unknown <sjc1b@hotmail.es>
2019-10-09 17:54:30 +0200

    tests

--- tests/test_frontier.py
+++ tests/test_frontier.py
@@ -1,13 +1,13 @@
 import pytest
-from cube.Frontier import Frontier
+from ..cube.Cube import Frontier
 import time
+import os
 from cube.Cube import Cube
 from copy import deepcopy
 
-test_file = '../resources/test.json'
 
 def test_force_data_structure():
-    
+    test_file = '../resources/test.json'
     try:
         frontier = Frontier()
         x = Cube(test_file)
@@ -19,8 +19,12 @@ def test_force_data_structure():
     except:
         end_time = time.time()
         
-    
     total_time = end_time - begin_time
     print(total_time)
     
-
+def main():
+    test_force_data_structure()
+    
+if __name__ == "__main__":
+    main()
+    pass
