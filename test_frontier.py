f381d66452baa4cc8c05f9ca172679f316534ec1
unknown <sjc1b@hotmail.es>
2019-10-13 13:33:48 +0200

    Data structure test finished

--- test_frontier.py
+++ test_frontier.py
@@ -1,30 +1,25 @@
 from src.Frontier import Frontier
 import time
 import os
-import src.Cube
+from src.Cube import Cube
 
 
 def test_force_data_structure():
     test_file = 'src/resources/test.json'
     begin_time = 0
+    
     try:
         frontier = Frontier()
         x = Cube(test_file)
-        print(x)
-        print('hemos llegado aqui')
-        x.move('l1')
-        print('hemos hecho el move')
+        x.move("l2")
         begin_time = time.time()
-        print(begin_time)
-        print('uwu before')
+       
         while True:
-            print('uwu')
             y=x.clone()
             frontier.Insert(y)
     except:
         end_time = time.time()
-        print('hola')
-        
+ 
     total_time = end_time - begin_time
     print(total_time)
     
