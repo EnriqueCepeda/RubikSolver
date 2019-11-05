77142e74d06234fc263a4aa6b6b51b05536ec196
Enrique Cepeda <ecepedavillamayor@gmail.com>
2019-11-05 13:05:54 +0100

    class changes

--- src/Search.py
+++ src/Search.py
@@ -22,8 +22,8 @@ class SearchStrategies:
             return node.cost
         elif self.strategy == "BFS":
             return 1 + node.node_depth
-        elif self.strategy in {"IDS", "DFS", "DLS"}:
-            return 1 / (1 + node.node_depth)
+        elif self.strategy in {"IDS", "DLS"}:
+            return -node.node_depth
 
     def solution(self, node):
         """This method creates in a stack the path solution of the problem after applying the 
@@ -46,17 +46,19 @@ class SearchStrategies:
     def concrete_search(self, limit):
         frontier = Frontier_SortedList.Frontier_SortedList()
         closed = {}
-        initial_node = TreeNode.TreeNode(
-            self.problem.initial_state, 0, 0, None, None)
+        initial_node = TreeNode.TreeNode(self.problem.initial_state, 0, 0, None, None)
         initial_node.f = self.__f_strategy(initial_node)
         frontier.insert(initial_node)
         solution = False
+        counter = 0
         while not solution and not frontier.is_empty():
+            counter += 1
+            print(counter)
             actual_node = frontier.remove()
             print(actual_node.state.create_md5())
-            print("actual node f: "+str(actual_node.f))
-            print("actual node depth: "+str(actual_node.node_depth))
-            print("actual node cost: "+str(actual_node.cost))
+            print("actual node f: " + str(actual_node.f))
+            print("actual node depth: " + str(actual_node.node_depth))
+            print("actual node cost: " + str(actual_node.cost))
             pruned = False
             if self.problem.is_goal(actual_node.state):
                 solution = True
@@ -65,10 +67,14 @@ class SearchStrategies:
                     if actual_node.state.create_md5() not in closed.keys():
                         closed[actual_node.state.create_md5()] = actual_node.f
                     else:
-                        pruned = True
+                        if abs(actual_node.f) < abs(
+                            closed[actual_node.state.create_md5()]
+                        ):
+                            closed[actual_node.state.create_md5()] = actual_node.f
+                        else:
+                            pruned = True
                 if actual_node.node_depth < limit and not pruned:
-                    successors = StateSpace.StateSpace.successors(
-                        actual_node.state)
+                    successors = StateSpace.StateSpace.successors(actual_node.state)
                     for successor in successors:
                         f = self.__f_strategy(actual_node)
                         treenode = TreeNode.TreeNode(
@@ -99,17 +105,20 @@ class SearchStrategies:
     def user_interface(self):
         print("-----INTELIGENT SYSTEMS - A1-02 PROJECT-----")
         print("Which strategy do you want to select? (UCS, BFS, DLS, IDS): ")
-        strategy = input()
-        while strategy.upper() not in {"UCS", "BFS", "DLS", "IDS"}:
+        strategy = input().upper()
+        while strategy not in {"UCS", "BFS", "DLS", "IDS", "DFS"}:
             print("Please, select a valid stategy.\n")
-            strategy = input()
+            strategy = input().upper()
+        if strategy == "DFS":
+            limit = 10000
+            strategy = "DLS"
+        else:
+            limit = self.ask_integer(
+                "Specify the limit of the strategy (An integer number greater than 0): ",
+                "Please, select a valid limit:\n",
+            )
 
-        limit = self.ask_integer(
-            "Specify the limit of the strategy (An integer number greater than 0): ",
-            "Please, select a valid limit:\n",
-        )
-
-        if strategy.upper() == "IDS":
+        if strategy == "IDS":
             increment = self.ask_integer(
                 "You are using IDS strategy, specify the limit (An integer number greater than 0): ",
                 "Please, select a valid increment.\n ",
@@ -124,12 +133,12 @@ class SearchStrategies:
             json_file_root = input()
 
         print("Do you want to do the search using the pruning technique? (Yes/No)")
-        pruning = input()
-        while pruning.upper() not in {"YES", "NO", "Y", "N"}:
+        pruning = input().upper()
+        while pruning not in {"YES", "NO", "Y", "N"}:
             print("Please, answer yes or no to the pruning question: ")
-            pruning = input()
+            pruning = input().upper()
 
-        pruning_boolean = pruning.upper() in {"YES", "Y"}
+        pruning_boolean = pruning in {"YES", "Y"}
 
         return strategy, limit, increment, json_file_root, pruning_boolean
 
@@ -150,11 +159,11 @@ class SearchStrategies:
 
 
 if __name__ == "__main__":
-    #strategy, limit, increment, json_path, pruning = SearchStrategies.user_interface()
-    #initial_cube = Cube(json_path)
-    initial_cube = Cube("src/resources/cube.json")
-    #search_object = SearchStrategies(initial_cube, strategy, limit, increment, pruning)
-    search_object = SearchStrategies(initial_cube, "BFS", 1, 1, True)
+    strategy, limit, increment, json_path, pruning = SearchStrategies.user_interface()
+    initial_cube = Cube(json_path)
+    # initial_cube = Cube("src/resources/cube.json")
+    search_object = SearchStrategies(initial_cube, strategy, limit, increment, pruning)
+    # search_object = SearchStrategies(initial_cube, "BFS", 1, 1, True)
     result = search_object.search()
     if result is not None:
         search_object.print_solution(result)
--- src/StateSpace.py
+++ src/StateSpace.py
@@ -1,10 +1,10 @@
 import Cube
 from copy import deepcopy
 
-class StateSpace:
 
+class StateSpace:
     @staticmethod
-    def successors(cube): 
+    def successors(cube):
         """Return the successors list of a cube
         
         Arguments:
@@ -13,16 +13,16 @@ class StateSpace:
         Returns:
             list -- Formed by a 3-tuples with: the movement, the successor cube and the movement cost
         """
-        possible_successors=[]
+        possible_successors = []
         for movement in cube.valid_movements():
             successor_cube = deepcopy(cube)
             successor_cube.move(movement)
-            possible_successors.append([movement,successor_cube,1])
+            possible_successors.append([movement, successor_cube, 1])
         return possible_successors
 
-    
-if __name__ == '__main__':
-    x = Cube.Cube('resources/cube.json')
+
+if __name__ == "__main__":
+    x = Cube.Cube("resources/cube.json")
     successor = StateSpace.successors(x)
     for element in successor:
         print(element[1].create_md5())
