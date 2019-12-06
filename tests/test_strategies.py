74774824d18b2ac92136b58c38f4584dfd68e225
Enrique Cepeda <ecepedavillamayor@gmail.com>
2019-12-06 16:47:17 +0100

    Project finished

--- tests/test_strategies.py
+++ tests/test_strategies.py
@@ -12,44 +12,7 @@ def test_pruning():
         id=0, state=cube, cost=0, node_depth=0, f=0, parent=None, last_action=None
     )
     closed = {treenode.state.create_md5(): treenode.f}
-    pruned = SearchStrategies.check_node_pruning(treenode, closed)
+    pruned = SearchStrategies.check_node_pruning_1st_prune(treenode, closed)
     assert pruned
 
-
-"""
-def test_UCS():
-    initial_cube = Cube.Cube(test_file)
-    search_UCS = SearchStrategies(initial_cube, "UCS", 2, 1, True)
-    result = search_UCS.search()
-    while not result.empty():
-        node = result.get()
-    assert node.node_depth == 2
-
-
-def test_DLS():
-    initial_cube = Cube.Cube(test_file)
-    search_UCS = SearchStrategies(initial_cube, "DLS", 2, 1, True)
-    result = search_UCS.search()
-    while not result.empty():
-        node = result.get()
-    assert node.node_depth == 2
-
-
-def test_IDS():
-    initial_cube = Cube.Cube(test_file)
-
-    search_IDS = SearchStrategies(initial_cube, "IDS", 10, 1, True)
-    solution_IDS = search_IDS.search()
-    while solution_IDS.not_empty:
-        node = solution_IDS.get()
-    IDS_solution_depth = node.node_depth
-    search_BFS = SearchStrategies(initial_cube, "BFS", 10, 1, True)
-    solution_BFS = search_BFS.search()
-    while solution_BFS.not_empty:
-        node = solution_BFS.get()
-    BFS_solution_depth = node.node_depth
-
-    assert IDS_solution_depth == BFS_solution_depth
-
-"""
 
